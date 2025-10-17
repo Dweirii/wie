8453d340c35b14947bcabfee71cbf67ab0a55f83
Zaid Dweiri <zaiddweiri962@gmail.com>
2025-10-17 19:21:28 +0300

    update the logo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -40,7 +40,7 @@ export default function AboutPage() {
             </div>
             <div className="bg-gray-50 rounded-2xl p-8">
               <img
-                src="/image.png"
+                src="/images/wie-logo.png"
                 alt="IEEE Global Network"
                 className="w-full h-auto rounded-lg"
               />
